5b39295d70d516c8eeefd2014510a61febfd3abe
Marzia Pirozzi <marz.pirozzi@studenti.unina.it>
2024-05-17 21:22:27 +0200

    Initial Commit

---
+++ MCFinal/MCFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCFinalApp.swift
+//  MCFinal
+//
+//  Created by Marzia Pirozzi on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MCFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
